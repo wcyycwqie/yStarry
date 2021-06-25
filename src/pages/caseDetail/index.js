301d404c0b6f1482b9f2683ebd690ea7484eeee0
wenchaoyue <584330116@qq.com>
2021-06-25 18:38:49 +0800

    06-25

--- src/pages/caseDetail/index.js
+++ src/pages/caseDetail/index.js
@@ -2,10 +2,11 @@
  * @Author: 
  * @Date: 2021-06-23 15:55:30
  * @LastEditors: Chaoyue
- * @LastEditTime: 2021-06-23 18:18:58
+ * @LastEditTime: 2021-06-25 17:41:49
  * @FilePath: \yStarry\src\pages\caseDetail\index.js
  */
 import React from 'react';
+import './index.min.css';
 
 class MainContent extends React.Component {
     constructor(props) {
@@ -18,8 +19,13 @@ class MainContent extends React.Component {
     render () {
         return (
             <div className="main">
-                <div className="content">
-                    hhohoho
+                <div className="details-area">
+                    <div className="area-title">title</div>
+                    <div className="area-describe">tag</div>
+                    <div className="area-showCard">
+                        <img src="http://www.yfunny.cool/image/01.png" alt="" />
+                        <div className="bottom-card"></div>
+                    </div>
                 </div>
             </div>
         )
@@ -31,8 +37,8 @@ export default function CaseDetail (props) {
     console.log(props);
     return (
         <div id="CaseDetail" className="CaseDetail">
-            <MainContent/>
+            <MainContent />
         </div>
     )
-
+ 
 }
